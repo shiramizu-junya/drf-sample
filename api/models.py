75f72e164628da6b0d50c4e38731da93c1c7dd50
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2026-01-24 09:38:36 +0900

    Productモデルのフィールドにverbose_nameを追加し、マイグレーションファイルを更新。管理サイトにProductモデルを登録。

--- api/models.py
+++ api/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.IntegerField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="製品名")
+    description = models.TextField(verbose_name="製品説明")
+    price = models.IntegerField(max_length=100, verbose_name="価格")
+
+    class Meta:
+        verbose_name_plural = "製品一覧"
 
     def __str__(self):
         return self.name
