4c8208437469f62196d2b6ce06203757cd014fd3
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2026-01-31 10:20:38 +0900

    不要なファイルと設定を削除し、プロジェクトをクリーンアップ

--- api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product, Order
-
-# Register your models here.
-
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.index_title = "管理サイトへようこそ"
-admin.site.site_header = "管理サイト"
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('products/', views.product_list, name='product-list'),
-    path('product/<int:pk>/', views.product, name='product-detail'),
-    path('orders/', views.order_list, name='order-list'),
-]
